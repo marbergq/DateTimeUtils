e951af38f459ab89780edffe0c8390a175171894
Martin Bergqvist <martin.bergqvist@tripoint.se>
2017-02-23 21:29:31 +0100

    Initial commit

---
+++ DateTimeUtils.Range/DateTimeUtils.Range/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DateTimeUtils.Range
+{
+    public class Class1
+    {
+    }
+}
---
+++ DateTimeUtils.Range/DateTimeUtils.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DateTimeUtils.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DateTimeUtils.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fef61f50-87da-4928-bb63-8fd55bcb84d2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
